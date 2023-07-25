55b863b030e4c2444ed7a5174edd16c8d5ba1db9
Cherch <b.cherchiglia@gmail.com>
2023-07-25 18:57:18 -0400

    inital setup

---
+++ bcportfolio/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react'
+import { Route, Routes } from 'react-router'
+import Nav from './components/Nav'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const getYear = () => {
+    return new Date().getFullYear()
+  }
+
+  return (
+    <div>
+    <header>
+      <Nav />
+    </header>
+
+    <br />
+
+    <main>
+
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          {count}
+        </button>
+      </div>
+      <br />
+      <footer className="footer">
+          © {getYear()} Cherch, all rights reserved --
+          <a className="footerLink" target="_blank" href="mailto:cherchofficial@gmail.com"> CONTACT</a>
+        </footer>
+    </main>
+  </div>
+
+  )
+}
+
+export default App
