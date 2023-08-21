0d8796b7a7f14cc340bc8a29f59d69dcc5bb8eef
Cherch <b.cherchiglia@gmail.com>
2023-08-21 16:08:38 -0400

    button fix

--- bcportfolio/src/pages/Portfolio.jsx
+++ bcportfolio/src/pages/Portfolio.jsx
@@ -160,7 +160,7 @@ const Portfolio = () => {
             </div>
             <br />
             <button className="scrollButton" onClick={scrollUp} ref={scrollallTheWayDownRef}>
-                beam
+                beam up
             </button>
         </div>
     )
